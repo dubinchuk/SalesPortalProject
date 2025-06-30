2b2228751e9b293813ca1e88f185d1a11644df45
dubinchuk <dubinchuk.e@gmail.com>
2025-06-30 23:24:45 +0300

    fix: fix customer delete service

--- playwright.config.ts
+++ playwright.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+  timeout: 90000,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     ['html', { open: 'never' }],
